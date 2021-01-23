738c67ce99c98c339a571f4a46367ceff3119cd5
gusstavo <gustavo.dcomp@gmail.com>
2021-01-23 14:52:43 -0300

    Added image upload tool

--- main.py
+++ main.py
@@ -1,6 +1,66 @@
-from flask import Flask
+import imghdr
+import os
+import shutil
+
+from flask import (Flask, abort, redirect, render_template, request,
+                   send_from_directory, url_for)
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
+def create_app():
+    app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+    app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+    app.config['UPLOAD_PATH'] = 'static/uploaded_images'
+    remove_all_images(app.config['UPLOAD_PATH'])
+    app.run(host='127.0.0.1',port=5000,use_reloader=False,debug=True)
+
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(uploaded_file.stream):
+            return "Invalid image", 400
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return '', 204
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+def remove_all_images(dir):
+    folder = dir
+    for filename in os.listdir(folder):
+        if ".gitkeep" in filename:
+            continue
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+if __name__ == "__main__":
+    create_app()
